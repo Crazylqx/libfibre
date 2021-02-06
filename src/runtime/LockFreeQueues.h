939f42353f893aee7e9ca4d5269f5efeb3470916
Martin Karsten <mkarsten@uwaterloo.ca>
2021-02-05 22:26:08 -0500

    - updated copyright years

--- src/runtime/LockFreeQueues.h
+++ src/runtime/LockFreeQueues.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-    Copyright (C) Martin Karsten 2015-2020
+    Copyright (C) Martin Karsten 2015-2021
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
