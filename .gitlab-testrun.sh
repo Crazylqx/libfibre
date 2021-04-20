Crazylqx/libfibre

commit 5bb915c7463eb195adbdcd5273419e6227fbcc86

- refactor container data structures

src/runtime/LockFreeQueues.h
@@ -17,37 +17,30 @@
 #ifndef _LockFreeQueues_h_
 #define _LockFreeQueues_h_ 1
 
+#include "runtime/ContainerLink.h"
+
 // https://doi.org/10.1145/103727.103729
 // the MCS queue can be used to construct an MCS lock or the Nemesis queue
 // next() might stall, if the queue contains one element and the producer of a second elements waits before setting 'prev->vnext'
 template<typename Node, Node* volatile&(*Next)(Node&)>
 class QueueMCS {
   Node* volatile tail;
+
 public:
   QueueMCS() : tail(nullptr) {}
-  bool empty() const { return tail == nullptr; }
-
-  static void clear(Node& elem) {
-#if TESTING_ENABLE_ASSERTIONS
-    Next(elem) = nullptr;
-#endif
-  }
+  bool empty() const { return !tail; }
 
   bool tryPushEmpty(Node& first, Node& last) {
+    RASSERT(!Next(last), FmtHex(Next(last)));  // assume link invalidated at pop
     if (!empty()) return false;
-#if !TESTING_ENABLE_ASSERTIONS
-    Next(last) = nullptr;
-#endif
     Node* expected = nullptr;
-    return  __atomic_compare_exchange_n(&tail, &expected, &last, false, __ATOMIC_SEQ_CST, __ATOMIC_RELAXED);
+    return __atomic_compare_exchange_n(&tail, &expected, &last, false, __ATOMIC_SEQ_CST, __ATOMIC_RELAXED);
   }
 
   bool tryPushEmpty(Node& elem) { return tryPushEmpty(elem, elem); }
 
   Node* push(Node& first, Node& last) {
-#if !TESTING_ENABLE_ASSERTIONS
-    Next(last) = nullptr;
-#endif
+    RASSERT(!Next(last), FmtHex(&last));       // assume link invalidated at pop
     // make sure writes to 'next' are not reordered with this update
     Node* prev = __atomic_exchange_n(&tail, &last, __ATOMIC_SEQ_CST); // swing tail to last of new element(s)
     if (prev) Next(*prev) = &first;
@@ -70,6 +63,7 @@ public:
 template<typename Node, Node* volatile&(*Next)(Node&)>
 class QueueNemesis : public QueueMCS<Node,Next> {
   Node* volatile head;
+
 public:
   QueueNemesis() : head(nullptr) {}
 
@@ -92,7 +86,7 @@ public:
       next = QueueMCS<Node,Next>::next(*elem); // memory sync in next()
       if (next) head = next;
     }
-    QueueMCS<Node,Next>::clear(*elem);
+    Next(*elem) = nullptr;                     // invalidate link
     return elem;
   }
 
@@ -101,5 +95,80 @@ public:
     return pop(dummy);
   }
 };
+
+// http://doc.cat-v.org/inferno/concurrent_gc/concurrent_gc.pdf
+// https://www.cs.rice.edu/~johnmc/papers/cqueues-mellor-crummey-TR229-1987.pdf
+// http://www.1024cores.net/home/lock-free-algorithms/queues/intrusive-mpsc-node-based-queue
+// https://github.com/samanbarghi/MPSCQ/blob/master/src/MPSCQueue.h
+
+// NOTE WELL: Static cast from Link to Node must work! If downcasting,
+//            Link must the first class that Node inherits from.
+template<typename Node, typename Link, Node* volatile&(*Next)(Node&), bool Blocking>
+class QueueStub {
+  Link anchorLink;
+  Node* stub;
+  Node* head;
+  Node* tail;
+
+  // peek/pop operate in chunks of elements and re-append stub after each chunk
+  // after re-appending stub, tail points to stub, if no further insertions -> empty!
+  bool checkStub() {
+    if (head == stub) {                             // if current front chunk is empty
+      if (Blocking) {                               // BLOCKING:
+        Node* expected = stub;                      //   check if tail also points at stub -> empty?
+        Node* xchg = (Node*)(uintptr_t(expected) | 1);    //   if yes, mark queue empty
+        bool empty = __atomic_compare_exchange_n((Node**)&tail, &expected, xchg, false, __ATOMIC_SEQ_CST, __ATOMIC_RELAXED);
+        if (empty) return false;                    //   queue is empty and is marked now
+        if (uintptr_t(expected) & 1) return false;  //   queue is empty and was marked before
+      } else {                                      // NONBLOCKING:
+        if (tail == stub) return false;             //   check if tail also points at stub -> empty?
+      }
+      while (!Next(*stub)) Pause();                 // producer in push()
+      head = Next(*stub);                           // remove stub
+      push(*stub);                                  // re-append stub at end
+    }
+    return true;
+  }
+
+public:
+  QueueStub() : stub(static_cast<Node*>(&anchorLink)) {
+    head = tail = Next(*stub) = stub;
+    if (Blocking) tail = (Node*)(uintptr_t(tail) | 1); // mark queue empty
+  }
+  bool empty() const { return (head == stub && tail == stub); }
+
+  bool push(Node& first, Node& last) {
+    RASSERT(!Next(last), FmtHex(&last));               // assume link invalidated at pop
+    Node* prev = __atomic_exchange_n((Node**)&tail, &last, __ATOMIC_SEQ_CST); // swing tail to last of new element(s)
+    bool empty = false;
+    if (Blocking) {                                    // BLOCKING:
+      empty = uintptr_t(prev) & 1;                     //   check empty marking
+      prev = (Node*)(uintptr_t(prev) & ~uintptr_t(1)); //   clear marking
+    }
+    Next(*prev) = &first;                              // append segments to previous tail
+    return empty;
+  }
+
+  bool push(Node& elem) { return push(elem, elem); }
+
+  Node* peek() {
+    if (!checkStub()) return nullptr;
+    return head;
+  }
+
+  template<bool Peeked = false>
+  Node* pop() {
+    if (!Peeked && !checkStub()) return nullptr;
+    Node* retval = head;                               // head will be returned
+    while (!Next(*head)) Pause();                      // producer in push()
+    head = Next(*head);                                // remove head
+    Next(*retval) = nullptr;                           // invalidate link
+    return retval;
+  }
+};
+
+template<typename T, size_t NUM=0, size_t CNT=1, typename LT=SingleLink<T,CNT>> struct IntrusiveQueueNemesis : public QueueNemesis<T,LT::template VNext<NUM>> {};
+// NOTE WELL: The intrusive design leads to downcasting in the QueueStub class. This only works, if LT is the first class that T inherits from.
+template<typename T, size_t NUM=0, size_t CNT=1, typename LT=SingleLink<T,CNT>, bool Blocking=false> struct IntrusiveQueueStub : public QueueStub<T,LT,LT::template VNext<NUM>,Blocking> {};
 
 #endif /* _LockFreeQueues_h_ */

src/runtime/BaseProcessor.h
@@ -81,7 +81,7 @@ class BaseProcessor;
 typedef IntrusiveList<BaseProcessor,0,2> ProcessorList;
 typedef IntrusiveRing<BaseProcessor,1,2> ProcessorRing;
 
-class BaseProcessor : public ProcessorRing::Link {
+class BaseProcessor : public DoubleLink<BaseProcessor,2> {
   inline StackContext* tryLocal();
 #if TESTING_LOADBALANCING
   inline StackContext* tryStage();

src/runtime/Container.h
@@ -1,0 +1,279 @@
+/******************************************************************************
+    Copyright (C) Martin Karsten 2015-2021
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+#ifndef _Container_h_
+#define _Container_h_ 1
+
+#include "runtime/Assertions.h"
+#include "runtime/ContainerLink.h"
+
+#define Sentinel (Node*)0xdeadbeef
+
+template<typename Node, Node*&(*Next)(Node&)>
+class Stack {
+  Node* head;
+
+public:
+  Stack() : head(Sentinel) {}
+  bool empty() const { return head == Sentinel; }
+
+  Node*              front()       { return head; }
+  const Node*        front() const { return head; }
+
+  static Node*       next(      Node& elem) { return Next(elem); }
+  static const Node* next(const Node& elem) { return Next(elem); }
+
+  void push(Node& first, Node& last) {
+    RASSERT(!Next(last), FmtHex(Next(last))); // assume link invalidated at pop
+    Next(last) = head;
+    head = &first;
+  }
+
+  void push(Node& elem) {
+    push(elem, elem);
+  }
+
+  Node* pop() {
+    RASSERT(!empty(), FmtHex(this));
+    Node* last = head;
+    head = Next(*last);
+    Next(*last) = nullptr;               // invalidate link
+    return last;
+  }
+
+  Node* pop(size_t& count) {             // returns pointer to last element popped
+    RASSERT(!empty(), FmtHex(this));
+    Node* last = head;
+    for (size_t i = 1; i < count; i += 1) {
+      if (Next(*last) == nullptr) count = i; // breaks loop and sets count
+      else last = Next(*last);
+    }
+    head = Next(*last);
+    Next(*last) = nullptr;               // invalidate link
+    return last;
+  }
+};
+
+template<typename Node, Node*&(*Next)(Node&)>
+class Queue {
+  Node* head;
+  Node* tail;
+
+  void popHelper(Node*& elem) {}
+
+  void popHelper(Node*& elem, size_t& count) {
+    for (size_t i = 1; i < count; i += 1) {
+      if (Next(*elem) == Sentinel) count = i; // breaks loop and sets count
+      else elem = Next(*elem);
+    }
+  }
+
+public:
+  Queue() : head(Sentinel), tail(Sentinel) {}
+  bool empty() const {
+    RASSERT((head == Sentinel) == (tail == Sentinel), FmtHex(this));
+    return head == Sentinel;
+  }
+
+  Node*              front()       { return head; }
+  const Node*        front() const { return head; }
+  Node*              back()        { return tail; }
+  const Node*        back()  const { return tail; }
+
+  static Node*       next(      Node& elem) { return Next(elem); }
+  static const Node* next(const Node& elem) { return Next(elem); }
+
+  void push(Node& first, Node& last) {
+    RASSERT(!Next(last), FmtHex(Next(last))); // assume link invalidated at pop
+    Next(last) = Sentinel; // validate link
+    if (empty()) head = &first;
+    else Next(*tail) = &first;
+    tail = &last;
+  }
+
+  void push(Node& elem) {
+    push(elem, elem);
+  }
+
+  template<typename...Args>
+  Node* pop(Args&... args) {
+    RASSERT(!empty(), FmtHex(this));
+    Node* elem = head;
+    popHelper(elem, args...);
+    head = Next(*elem);
+    if (tail == elem) tail = Sentinel;
+    Next(*elem) = nullptr; // invalidate link
+    return elem;
+  }
+};
+
+template<typename Node, Node*&(*Next)(Node&), Node*&(*Prev)(Node&)>
+class Ring {
+
+  static void take_out(Node& first, Node& last) {
+    RASSERT(Prev(first) && Next(first), FmtHex(&first));
+    RASSERT(Prev(last) && Next(last), FmtHex(&last));
+    Next(*Prev(first)) = Next(last);
+    Prev(*Next(last))  = Prev(first);
+  }
+
+  static void combine_before(Node& next, Node& first, Node&last) {
+    RASSERT(Prev(next) && Next(next), FmtHex(&next));
+    Next(last) = &next;
+    Next(*Prev(next)) = &first;
+    Prev(first) = Prev(next);
+    Prev(next) = &last;
+  }
+
+  static void combine_after(Node& prev, Node& first, Node& last) {
+    RASSERT(Prev(prev) && Next(prev), FmtHex(&prev));
+    Prev(first) = &prev;
+    Prev(*Next(prev)) = &last;
+    Next(last) = Next(prev);
+    Next(prev) = &first;
+  }
+
+public:
+  static Node*       next(      Node& elem) { return Next(elem); }
+  static const Node* next(const Node& elem) { return Next(elem); }
+  static Node*       prev(      Node& elem) { return Prev(elem); }
+  static const Node* prev(const Node& elem) { return Prev(elem); }
+
+  static void close(Node& first, Node& last) {
+    Prev(first) = &last;
+    Next(last) = &first;
+  }
+
+  static void close(Node& elem) {
+    close(elem, elem);
+  }
+
+  static void insert_before(Node& next, Node& first, Node&last) {
+    RASSERT(!Prev(first), FmtHex(&first)); // assume link invalidated at pop
+    RASSERT(!Next(last), FmtHex(&last));   // assume link invalidated at pop
+    combine_before(next, first, last);
+  }
+
+  static void insert_before(Node& next, Node& elem) {
+    insert_before(next, elem, elem);
+  }
+
+  static void insert_after(Node& prev, Node& first, Node& last) {
+    RASSERT(!Prev(first), FmtHex(Prev(first))); // assume link invalidated at pop
+    RASSERT(!Next(last), FmtHex(Next(last)));   // assume link invalidated at pop
+    combine_after(prev, first, last);
+  }
+
+  static void insert_after(Node& prev, Node& elem) {
+    insert_after(prev, elem, elem);
+  }
+
+  static Node* remove(Node& first, Node& last) {
+    take_out(first, last);
+    Prev(first) = Next(last) = nullptr;    // invalidate link
+    return &last;
+  }
+
+  static Node* remove(Node& elem) {
+    return remove(elem, elem);
+  }
+
+  static void join_before(Node& next, Node& first, Node&last) {
+    RASSERT(Prev(first) == &last, FmtHex(&first));
+    RASSERT(Next(last) == &first, FmtHex(&last));
+    combine_before(next, first, last);
+  }
+
+  static void join_before(Node& next, Node& elem) {
+    combine_before(next, elem, elem);
+  }
+
+  static void join_after(Node& prev, Node& first, Node&last) {
+    RASSERT(Prev(first) == &last, FmtHex(&first));
+    RASSERT(Next(last) == &first, FmtHex(&last));
+    combine_after(prev, first, last);
+  }
+
+  static void join_after(Node& prev, Node& elem) {
+    combine_after(prev, elem, elem);
+  }
+
+  static Node* split(Node& first, Node& last) {
+    take_out(first, last);
+    close(first, last);
+    return &last;
+  }
+
+  static Node* split(Node& elem) {
+    return split(elem, elem);
+  }
+};
+
+// NOTE WELL: Static cast from Link to Node must work! If downcasting,
+//            Link must the first class that Node inherits from.
+template<typename Node, typename Link, Node*&(*Next)(Node&), Node*&(*Prev)(Node&)>
+class List : public Ring<Node,Next,Prev> {
+  Link anchorLink;
+  Node* anchor;
+
+public:
+  List() : anchor(static_cast<Node*>(&anchorLink)) {
+    Next(*anchor) = Prev(*anchor) = anchor;
+  }
+
+  using Ring<Node,Next,Prev>::insert_before;
+  using Ring<Node,Next,Prev>::insert_after;
+  using Ring<Node,Next,Prev>::remove;
+
+  Node*       front()       { return Next(*anchor); }
+  const Node* front() const { return Next(*anchor); }
+  Node*       back()        { return Prev(*anchor); }
+  const Node* back()  const { return Prev(*anchor); }
+  Node*       edge()        { return anchor; }
+  const Node* edge()  const { return anchor; }
+
+  bool        empty() const {
+    RASSERT((front() == edge()) == (back() == edge()), FmtHex(this));
+    return front() == edge();
+  }
+
+  Node* remove(Node& first, size_t& count) {
+    RASSERT(Prev(first) && Next(first), FmtHex(&first));
+    Node* last = &first;
+    for (size_t i = 1; i < count; i += 1) {
+      if (Next(*last) == edge()) count = i; // breaks loop and sets count
+      else last = Next(*last);
+    }
+    return remove(first, *last);
+  }
+
+  Node* removeAll() { return remove(*front(), *back()); }
+
+  void push_front(Node& elem)               { insert_before(*front(), elem); }
+  void push_back(Node& elem)                { insert_after (*back(),  elem); }
+  void splice_back(Node& first, Node& last) { insert_after (*back(),  first, last); }
+
+  Node* pop_front() { RASSERT(!empty(), FmtHex(this)); return remove(*front()); }
+  Node* pop_back()  { RASSERT(!empty(), FmtHex(this)); return remove(*back()); }
+};
+
+template<typename T, size_t NUM=0, size_t CNT=1, typename LT=SingleLink<T,CNT>> struct IntrusiveStack : public Stack<T,LT::template Next<NUM>> {};
+template<typename T, size_t NUM=0, size_t CNT=1, typename LT=SingleLink<T,CNT>> struct IntrusiveQueue : public Queue<T,LT::template Next<NUM>> {};
+template<typename T, size_t NUM=0, size_t CNT=1, typename LT=DoubleLink<T,CNT>> struct IntrusiveRing : public Ring<T,LT::template Next<NUM>,LT::template Prev<NUM>> {};
+// NOTE WELL: The intrusive design leads to downcasting in the List class. This only works, if LT is the first class that T inherits from.
+template<typename T, size_t NUM=0, size_t CNT=1, typename LT=DoubleLink<T,CNT>> struct IntrusiveList : public List<T,LT,LT::template Next<NUM>,LT::template Prev<NUM>> {};
+
+#endif /* _Container_h_ */

src/runtime/ContainerLink.h
@@ -1,0 +1,58 @@
+/******************************************************************************
+    Copyright (C) Martin Karsten 2015-2021
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+#ifndef _ContainerLink_h_
+#define _ContainerLink_h_ 1
+
+#include "runtime/Platform.h"
+
+template<typename T,size_t CNT=1> class SingleLink {
+  struct {
+    union {
+      T* next;
+      T* volatile vnext;
+    };
+  } link[CNT];
+  template<size_t NUM=0> static void check() { static_assert(NUM < CNT, "NUM >= CNT"); }
+public:
+  SingleLink() { for (size_t i = 0; i < CNT; i++) link[i].next = nullptr; }
+  template<size_t NUM=0>        inline bool         valid() const { check<NUM>(); return link[NUM].vnext; }
+  template<size_t NUM=0> static inline T*&          Next(T& elem) { check<NUM>(); return elem.link[NUM].next; }
+  template<size_t NUM=0> static inline T*volatile& VNext(T& elem) { check<NUM>(); return elem.link[NUM].vnext; }
+};
+
+template<typename T,size_t CNT=1> class DoubleLink {
+  struct {
+    union {
+      T* next;
+      T* volatile vnext;
+    };
+    union {
+      T* prev;
+      T* volatile vprev;
+    };
+  } link[CNT];
+  template<size_t NUM=0> static void check() { static_assert(NUM < CNT, "NUM >= CNT"); }
+public:
+  DoubleLink() { for (size_t i = 0; i < CNT; i++) link[i].next = link[i].prev = nullptr; }
+  template<size_t NUM=0>        inline bool         valid() const { check<NUM>(); return link[NUM].vnext; }
+  template<size_t NUM=0> static inline T*&          Next(T& elem) { check<NUM>(); return elem.link[NUM].next; }
+  template<size_t NUM=0> static inline T*volatile& VNext(T& elem) { check<NUM>(); return elem.link[NUM].vnext; }
+  template<size_t NUM=0> static inline T*&          Prev(T& elem) { check<NUM>(); return elem.link[NUM].prev; }
+  template<size_t NUM=0> static inline T*volatile& VPrev(T& elem) { check<NUM>(); return elem.link[NUM].vprev; }
+};
+
+#endif /* _ContainerLink_h_ */

commit d1b63da5d1a9e6122269c6783d5585859bc48652

- fix bug in stub queue (introduced in 5bb915c7463eb195adbdcd5273419e6227fbcc86) - fix corresponding problem in CI/CD test script, which masked bug

.gitlab-testrun.sh
@@ -53,7 +53,7 @@ function post() {
 }
 
 function run_memcached() {
-	for ((i=0;i<5;i+=1)); do
+	for ((i=0;i<3;i+=1)); do
 		FibrePrintStats=1 $TASKSET_SERVER memcached/memcached -t $count -b 16384 -c 32768 -m 10240 -o hashpower=24 &
 		sleep 3
 		mutilate -s0 -r 100000 -K fb_key -V fb_value --loadonly
@@ -138,7 +138,7 @@ function run_6() {
 }
 
 function prep_7() {
-	sed -i -e 's/.* TESTING_NEMESIS_READYQUEUE .*/#undef TESTING_NEMESIS_READYQUEUE/' src/runtime-glue/testoptions.h
+	sed -i -e 's/.* TESTING_NEMESIS_READYQUEUE .*/#undef TESTING_NEMESIS_READYQUEUE/' src/runtime/testoptions.h
 	echo memcached
 }
 

src/runtime/LockFreeQueues.h
@@ -125,6 +125,7 @@ class QueueStub {
       }
       while (!Next(*stub)) Pause();                 // producer in push()
       head = Next(*stub);                           // remove stub
+      Next(*stub) = nullptr;                        // invalidate link
       push(*stub);                                  // re-append stub at end
     }
     return true;
@@ -132,7 +133,8 @@ class QueueStub {
 
 public:
   QueueStub() : stub(static_cast<Node*>(&anchorLink)) {
-    head = tail = Next(*stub) = stub;
+    head = tail = stub;
+    Next(*stub) = nullptr;                             // invalidate link
     if (Blocking) tail = (Node*)(uintptr_t(tail) | 1); // mark queue empty
   }
   bool empty() const { return (head == stub && tail == stub); }