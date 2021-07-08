Crazylqx/libfibre

commit 981444ec4d56c3145f083e2089366c125705fb8f

- reorganize affinity management & transient vs. sticky stealing - add TESTING_CLUSTER_POLLER_FLOAT - new default configuration:   TESTING_CLUSTER_POLLER_FLOAT 1   TESTING_ONESHOT_REGISTRATION 1

src/runtime/Fred.h
@@ -59,12 +59,19 @@ typedef FredMPSC<FredReadyLink,false> FredReadyQueue;
 class Fred : public DoubleLink<Fred,FredLinkCount> {
 public:
   enum Priority : size_t { TopPriority = 0, DefPriority = 1, LowPriority = 2, NumPriority = 3 };
+  enum Affinity : size_t { NoAffinity = 0, TempAffinity = 1, FixedAffinity = 2 };
+
+#if TESTING_DEFAULT_AFFINITY
+static const Affinity DefAffinity = TempAffinity;
+#else
+static const Affinity DefAffinity = NoAffinity;
+#endif
 
 private:
   vaddr          stackPointer; // holds stack pointer while stack inactive
   BaseProcessor* processor;    // next resumption on this processor
   Priority       priority;     // scheduling priority
-  bool           affinity;     // affinity prohibits re-staging
+  Affinity       affinity;     // affinity to worker
 
   enum RunState : size_t { Parked = 0, Running = 1, ResumedEarly = 2 };
   RunState volatile runState;    // 0 = parked, 1 = running, 2 = early resume
@@ -95,8 +102,8 @@ private:
 
 protected:
   // constructor/destructors can only be called by derived classes
-  Fred(BaseProcessor& proc, bool affinity = false); // main constructor
-  Fred(Scheduler&, bool background = false);        // uses delegation
+  Fred(BaseProcessor& proc, Affinity affinity = DefAffinity); // main constructor
+  Fred(Scheduler&, bool background = false);                  // uses delegation
   ~Fred() { RASSERT(runState == Running, FmtHex(this), runState); }
 
   void initStackPointer(vaddr sp) {
@@ -175,8 +182,13 @@ public:
   }
 
   // hard affinity - no staging
-  bool getAffinity()          { return affinity; }
-  Fred* setAffinity(bool a)   { affinity = a; return this; }
+  Affinity getAffinity()          { return affinity; }
+  Fred* setAffinity(Affinity a)   { affinity = a; return this; }
+  bool checkAffinity() {
+    if (affinity == FixedAffinity) return true;
+    if (affinity == TempAffinity) affinity = FixedAffinity;
+    return false;
+  }
 
   // priority
   Priority getPriority() const  { return priority; }

.gitlab-testrun.sh
@@ -105,7 +105,8 @@ function prep_4() {
 }
 
 function prep_5() {
-	sed -i -e 's/.* TESTING_ONESHOT_REGISTRATION .*/#define TESTING_ONESHOT_REGISTRATION 1/' src/runtime-glue/testoptions.h
+	sed -i -e 's/.* TESTING_CLUSTER_POLLER_FLOAT .*/#undef TESTING_CLUSTER_POLLER_FLOAT/' src/runtime-glue/testoptions.h
+	sed -i -e 's/.* TESTING_ONESHOT_REGISTRATION .*/#undef TESTING_ONESHOT_REGISTRATION/' src/runtime-glue/testoptions.h
 	echo memcached
 }
 

src/libfibre/Fibre.h
@@ -176,7 +176,7 @@ public:
 
   /** Constructor setting affinity to processor. */
   Fibre(BaseProcessor &sp, size_t size = 0, size_t guard = 0)
-  : Fred(sp, true), stackSize(stackAlloc(size, guard)) { initDebug(); }
+  : Fred(sp, Fred::FixedAffinity), stackSize(stackAlloc(size, guard)) { initDebug(); }
 
   /** Constructor to immediately start fibre with `func(arg)`. */
   Fibre(funcvoid1_t func, ptr_t arg, bool background = false)

src/libfibre/Poller.cc
@@ -84,10 +84,14 @@ void PollerFibre::pollLoopSetup(PollerFibre* This) {
   This->pollLoop();
 }
 
-PollerFibre::PollerFibre(EventScope& es, BaseProcessor& proc, cptr_t parent, bool bg)
+PollerFibre::PollerFibre(EventScope& es, BaseProcessor& proc, cptr_t parent, bool cluster)
 : BasePoller(es, parent, "PollerFibre") {
   pollFibre = new Fibre(proc);
-  if (bg) pollFibre->setPriority(Fred::LowPriority);
+#if TESTING_CLUSTER_POLLER_FLOAT
+  if (cluster) pollFibre->setAffinity(Fred::NoAffinity);
+#else
+  if (cluster) pollFibre->setPriority(Fred::LowPriority);
+#endif
 }
 
 PollerFibre::~PollerFibre() {

src/libfibre/Poller.h
@@ -106,7 +106,7 @@ class PollerFibre : public BasePoller {
   static void pollLoopSetup(PollerFibre*);
 
 public:
-  PollerFibre(EventScope&, BaseProcessor&, cptr_t parent, bool bg = true);
+  PollerFibre(EventScope&, BaseProcessor&, cptr_t parent, bool cluster = true);
   ~PollerFibre();
   void start();
 };

src/runtime-glue/testoptions.h
@@ -2,8 +2,9 @@
 
 //#define TESTING_WORKER_POLLER         1 // poll events directly from each worker
 #define TESTING_CLUSTER_POLLER_FIBRE  1 // per-cluster poller(s): fibre vs. pthread
+#define TESTING_CLUSTER_POLLER_FLOAT  1 // per-cluster poller fibres(s): float vs. background
 //#define TESTING_POLLER_FIBRE_SPIN 65536 // poller fibre: spin loop of NB polls
-//#define TESTING_ONESHOT_REGISTRATION  1 // use oneshot event polling
+#define TESTING_ONESHOT_REGISTRATION  1 // use oneshot event polling
 
 /******************************** lock options ********************************/
 

src/runtime/BaseProcessor.cc
@@ -30,7 +30,7 @@ inline Fred* BaseProcessor::tryStage() {
   Fred* f = scheduler.stage();
   if (f) {
     DBG::outl(DBG::Level::Scheduling, "tryStage: ", FmtHex(this), ' ', FmtHex(f));
-    if (f->getAffinity()) {
+    if (f->checkAffinity()) {
       stats->borrow.count();
     } else {
       stats->stage.count();
@@ -48,16 +48,12 @@ inline Fred* BaseProcessor::trySteal() {
     Fred* f = victim->readyQueue.tryDequeue();
     if (f) {
       DBG::outl(DBG::Level::Scheduling, "trySteal: ", FmtHex(this), ' ', FmtHex(f));
-#if TESTING_STICKY_STEALING
-      if (f->getAffinity()) {
+      if (f->checkAffinity()) {
         stats->borrow.count();
       } else {
         stats->steal.count();
         f->changeProcessor(*this, _friend<BaseProcessor>());
       }
-#else
-      stats->steal.count();
-#endif
       return f;
     }
   }
@@ -82,9 +78,7 @@ void BaseProcessor::idleLoop() {
     Fred* nextFred = scheduler.loadManager.getReadyFred(*this);
     if (nextFred) {
       stats->handover.count();
-#if TESTING_STICKY_STEALING
-      if (!nextFred->getAffinity()) nextFred->changeProcessor(*this, _friend<BaseProcessor>());
-#endif
+      if (!nextFred->checkAffinity()) nextFred->changeProcessor(*this, _friend<BaseProcessor>());
     } else {
       do nextFred = scheduleInternal(); while (!nextFred);
     }

src/runtime/Fred.cc
@@ -18,15 +18,15 @@
 #include "runtime/Fred.h"
 #include "runtime-glue/RuntimeFred.h"
 
-Fred::Fred(BaseProcessor& proc, bool affinity)
+Fred::Fred(BaseProcessor& proc, Affinity affinity)
 : stackPointer(0), processor(&proc), priority(DefPriority), affinity(affinity), runState(Running) {
 #if TESTING_SHARED_READYQUEUE
-  this->affinity = true;
+  this->affinity = FixedAffinity;
 #endif
 }
 
 Fred::Fred(Scheduler& scheduler, bool background)
-: Fred(scheduler.placement(_friend<Fred>(), background), background) {}
+: Fred(scheduler.placement(_friend<Fred>(), background), background ? FixedAffinity : DefAffinity) {}
 
 template<Fred::SwitchCode Code>
 inline void Fred::switchFred(Fred& nextFred) {
@@ -156,7 +156,7 @@ void Fred::terminate() {
 }
 
 void Fred::rebalance() {
-  if (!affinity) processor = &Context::CurrProcessor().getScheduler().placement(_friend<Fred>(), true);
+  if (affinity != FixedAffinity) processor = &Context::CurrProcessor().getScheduler().placement(_friend<Fred>(), true);
 }
 
 // migrate to scheduler; clear affinity
@@ -167,7 +167,7 @@ void Fred::migrateNow(Scheduler& scheduler) {
 // migrate to proessor; clear affinity
 void Fred::migrateNow(BaseProcessor& proc) {
   Fred* f = Context::CurrFred();
-  f->affinity = false;
+  f->affinity = DefAffinity;
   f->processor = &proc;
   f->yieldForce();
 }

commit d5eee75442a1203abf62e8cf7af81749112f746e

- fix bug introduced in 981444ec4d56c3145f083e2089366c125705fb8f

src/runtime/Fred.h
@@ -62,7 +62,11 @@ public:
   enum Affinity : size_t { NoAffinity = 0, TempAffinity = 1, FixedAffinity = 2 };
 
 #if TESTING_DEFAULT_AFFINITY
+#if TESTING_PLACEMENT_STAGING
 static const Affinity DefaultAffinity = TempAffinity;
+#else
+static const Affinity DefaultAffinity = FixedAffinity;
+#endif
 #else
 static const Affinity DefaultAffinity = NoAffinity;
 #endif